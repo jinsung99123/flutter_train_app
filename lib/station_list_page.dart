a95ab7de85c230c8a16dabfab84965b4b630e852
손진성 <felixson99@gmail.com>
2025-03-31 17:45:47 +0900

    정류장 페이지 기초 틀 구현현

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -5,13 +5,61 @@ class StationListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
-        title:Row(
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('출발역')
-          ],
-        )
+          children: [Text('출발역')],
+        ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('수서')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),    
+              ListTile(title: Text('동탄')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('평택지제')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('천안아산')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('오송')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('대전')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('김천구미')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('동대구')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('경주')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+              ListTile(title: Text('울산')),
+              SizedBox(
+                width: double.infinity,
+                child:Divider(color: Colors.red,thickness: 2,)),
+            ],
+          ),
+        ],
       ),
     );
   }
