94c4c22e8094b64fa90922d5cac11c766ab96c9d
손진성 <felixson99@gmail.com>
2025-03-31 12:37:11 +0900

    메인화면 위젯 틀 구현현

--- lib/main.dart
+++ lib/main.dart
@@ -19,28 +19,63 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Card(
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Text('출발역'),
-                    Text('수서')                                         
-                  ],                  
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('기차 예매')
+          ],
+        )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [          
+              Container(
+                width:double.infinity,
+                height:180,
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,      
+                        
+                                       
+                        children: [                    
+                          Column(                          
+                            children: [
+                              Text('출발역'),
+                              Text('수서',style: TextStyle(fontSize: 30))                                         
+                            ],                  
+                          ),
+                          Container(
+                            height: 50,
+                            width: 150,
+                            child: VerticalDivider(color: Colors.red,thickness: 2),
+                          ),
+                          
+                          Column(
+                            children: [
+                              Text('도착역'),
+                              Text('부산',style: TextStyle(fontSize: 30),)                                         
+                            ],),
+                        ],
+                      ),
+                    ],
+                  )
                 ),
-                Column(
-                  children: [
-                    Text('도착역'),
-                    Text('부산')                                         
-                  ],),
-              ],
-            )
-          ),  
-          ElevatedButton(onPressed: (){}, child: Text('좌석 선택'))     
-        ]
+              
+            ),  
+            SizedBox(height: 10),
+            SizedBox(
+              width:double.infinity,              
+              child: ElevatedButton(onPressed: (){}, child: Text('좌석 선택',style: TextStyle(
+                fontSize:15
+              ),)))                  
+          ]
+        ),
       )
     );
   }
