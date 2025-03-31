0cb3ca9702232ea62a3906bb283c03405ac64d89
손진성 <felixson99@gmail.com>
2025-03-31 19:19:59 +0900

    좌석  예매 페이지 기초 틀 구현

--- lib/seat_page/seat_page.dart
+++ lib/seat_page/seat_page.dart
@@ -12,7 +12,112 @@ class SeatPage extends StatelessWidget {
           ],
         ),
       ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('수서'),
+              Icon(Icons.arrow_right_alt_rounded),
+              Text('부산')
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.purpleAccent,
+                        borderRadius: BorderRadius.circular(10))),
+                    Text('선택됨')
+                  ],               
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10))),
+
+                    
+                    Text('선택 안 됨')
+                  ],
+                ),
+              )
+              
+              
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),
+              seat(),])
+            ],
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('예매 하기'))
+        ],
+      ),
     );
   }
+}
 
+Widget seat() {
+  return Row(
+    children: [
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(color: Colors.grey,
+        borderRadius: BorderRadius.circular(10)),
+      ),
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(color: Colors.grey,
+        borderRadius: BorderRadius.circular(10)),
+      ),
+      Container(
+        child: Text('1'),
+      ),
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(color: Colors.grey,
+        borderRadius: BorderRadius.circular(10)),
+      ),
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(color: Colors.grey,
+        borderRadius: BorderRadius.circular(10)),
+      ),
+    ],
+  );
 }
